079df1403c9503bc75d37bddbec8395cdad6f951
ertugrulsagdic <38252389+ertugrulsagdic@users.noreply.github.com>
2020-05-13 21:14:57 +0300

    initial

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+export default App = () => {
+  return (
+    <View>
+      <Text>Welcome to app</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+});
+
